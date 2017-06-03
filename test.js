49d2263dfee4ec38c3027c8767458bb9b4f5e77b
Ilya Ig. Petrov <ilyaigpetrov@gmail.com>
2017-06-03 23:42:33 +0500

    Add script for testing pac files

---
+++ test.js
@@ -0,0 +1,32 @@
+// 'use strict'; Let PAC-script decide for itself.
+
+const Fs = require('fs');
+const Benchmark = require('benchmark');
+
+const args = process.argv.slice(2);
+if (args.length !== 1) {
+  console.error('ARGS: ./buggy.pac');
+}
+const pacPath = args.pop();
+
+const pacEnv = Fs.readFileSync('./env.js');
+
+const executePac = function executePac(pacStr, host) {
+
+  return (new Function (
+    'url', 'host',
+    pacEnv + ';' + pacStr + '; return FindProxyForURL(url, host);')
+  )(`http://${host}`, host);
+
+}
+
+const pacStr = Fs.readFileSync(pacPath).toString();
+[
+  'navalny.com',
+  'yandex.ru'
+].forEach((host) => {
+
+  const result = executePac(pacStr, host);
+  console.log(host, result);
+
+});
